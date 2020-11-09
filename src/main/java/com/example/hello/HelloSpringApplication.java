fb90405108ab1299456f0b824534444c9f8993d8
casey.t.johnson <casey.t.johnson@CPX-C15Z975TL18.dir.svc.accenture.com>
2020-11-09 15:17:26 -0500

    Answer GET /hello?name=<name> with application/JSON response

--- src/main/java/com/example/hello/HelloSpringApplication.java
+++ src/main/java/com/example/hello/HelloSpringApplication.java
@@ -1,8 +1,12 @@
 package com.example.hello;
 
+import java.util.HashMap;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,14 +19,12 @@ public class HelloSpringApplication {
 		SpringApplication.run(HelloSpringApplication.class, args);
 	}
 	
-	@RequestMapping("/")
-	public static String index() {
-		return "Greetings from Spring Boot!";
-	}
-	
-	@RequestMapping("/hello")
-	public static String hello(@RequestParam(name="name", defaultValue="World") String name) {
-		return String.format("Hello, %s!", name);
+	@RequestMapping(value="/hello", method=RequestMethod.GET, produces=MediaType.APPLICATION_JSON_VALUE)
+	public static HashMap<String, String> hello(@RequestParam(name="name", defaultValue="World") String name) {
+		HashMap<String, String> resp = new HashMap<String, String>();
+		String helloString = String.format("Hello, %s!", name);
+		resp.put("response", helloString);
+		return resp;
 	}
 
 }
